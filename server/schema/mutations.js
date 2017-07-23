faad24ec31336dc1456b41b21a9be3eafc5bf0f8
timurista <timothy.urista@gmail.com>
2017-07-23 15:01:44 -0700

    added login and logout functions

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -16,10 +16,28 @@ const mutation = new GraphQLObjectType({
         password: { type: GraphQLString }
       },
       // request is request object coming from express
-      resolve(parentValue, args, req) {
+      resolve(parentValue, { email, password }, req) {
         return AuthService.signup({ email, password, req });
       }
-    }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout(); // from passportjs
+        return user;
+      }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.login({ email, password, req });
+      }
+    },
   }
 });
 
--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,10 +1,18 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLID } = graphql;
+const UserType = require('./user_type');
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    dummyFields: { type: GraphQLID }
+    dummyFields: { type: GraphQLID },
+    user: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        // if user is signed in, then req.user exists
+        return req.user;
+      }
+    }
   }
 });
 
